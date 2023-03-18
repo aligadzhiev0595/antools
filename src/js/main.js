b88594953646c1726c67b30fdc91161ef78ea169
aligadzhiev0595 <aligadzhiev0595@gmail.com>
2023-03-18 20:17:26 +0300

    feature: added modal windows and fastened the call functionality to the buttons located in the header

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,31 @@ link.forEach(el => {
     document.body.classList.remove('overflow')
   })
 })
+
+document.querySelector(".header__btn-login").onclick = function () {
+  document.querySelector(".overlay").classList.add("overlay_active");
+  document
+    .querySelector(".popup__form-login")
+    .classList.add("popup__form-login_active");
+    burger.classList.remove("active");
+    nav.classList.remove("active");
+    document.body.classList.remove('overflow')
+};
+document.querySelector(".header__btn-signup").onclick = function () {
+  document.querySelector(".overlay").classList.add("overlay_active");
+  document
+    .querySelector(".popup__form-signup")
+    .classList.add("popup__form-signup_active");
+    burger.classList.remove("active");
+    nav.classList.remove("active");
+    document.body.classList.remove('overflow')
+};
+document.querySelector(".popup__close").onclick = function () {
+  document.querySelector(".overlay").classList.remove("overlay_active");
+  document
+    .querySelector(".popup__form-login")
+    .classList.remove("popup__form-login_active");
+  document
+    .querySelector(".popup__form-signup")
+    .classList.remove("popup__form-signup_active");
+};
