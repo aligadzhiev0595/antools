65c11ba3da838b488e78f205c0dffd79de932293
aligadzhiev0595 <aligadzhiev0595@gmail.com>
2023-03-26 02:01:42 +0300

    fix: added his own slider implementation and replaced the library one

--- src/js/main.js
+++ src/js/main.js
@@ -69,18 +69,62 @@ document.querySelector(".tools__more").onclick = function () {
 };
 
 /**
- * brief: logic for swiper slider
+ * brief: logic for  sliders
  */
 
-let swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+const prev = document.querySelector(".btn-prev"),
+  next = document.querySelector(".btn-next"),
+  sliders = document.querySelectorAll(".slider-inner"),
+  dots = document.querySelectorAll(".dot");
+
+let index = 0;
+
+const activeSlide = (n) => {
+  for (slide of sliders) {
+    slide.classList.remove("active");
+  }
+  sliders[n].classList.add("active");
+
+};
+const activeDot = (n) => {
+  for (dot of dots) {
+    dot.classList.remove("active");
+  }
+  dots[n].classList.add("active");
+};
+
+const prepareCurrentSlide = (ind) => {
+  activeSlide(ind);
+  activeDot(ind);
+};
+
+const nextSlide = () => {
+  if (index == sliders.length - 1) {
+    index = 0;
+    prepareCurrentSlide(index);
+  } else {
+    index++;
+    prepareCurrentSlide(index);
+  }
+};
+const prevSlide = () => {
+  if (index == 0) {
+    index = sliders.length - 1;
+    prepareCurrentSlide(index);
+  } else {
+    index--;
+    prepareCurrentSlide(index);
+  }
+};
+
+dots.forEach((item, indexDot) => {
+  item.addEventListener("click", () => {
+    index = indexDot;
+    prepareCurrentSlide(index);
+  });
 });
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
+
+// setInterval(nextSlide, 2500)
