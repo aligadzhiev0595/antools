08e55760dd7de61f9b307853176e2cbba3599962
aligadzhiev0595 <aligadzhiev0595@gmail.com>
2023-04-15 20:49:37 +0300

    feature: added animation for home & tools sections

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,25 +1,92 @@
-export function animation(){
-	// const timeLine = gsap.timeLine()
-	// timeLine.fromTo(
-	// 	'.title',{
-	// 		x: -100,
-	// 		opacity:0
-	// 	},
-	// 	{
-	// 		x: 0,
-	// 		opacity:1,
-	// 		duration: 1
-	// 	}
-	// )
+export function animation() {
 	gsap.to('.home', {
-		scrollTrigger:{
-			trigger: '.tools',
-			start: '-350 0',
+		scrollTrigger: {
+			trigger: '.home',
+			start: '50 0',
 			scrub: true,
-			duration:1
+			// markers: true
 		},
-		yPercent:40,
-		xPercent: -100,
-		scale:0.5,
+		yPercent: 5,
+		duration: 1,
+	})
+
+	gsap.from('.tools__title', {
+		scrollTrigger: {
+			trigger: '.home',
+			start: '100 0',
+			scrub: true,
+			// markers: true
+		},
+		y: 100,
+		autoAlpha: 0,
+		duration: 1,
+	})
+
+	gsap.from('.tools__subtitle', {
+		scrollTrigger: {
+			trigger: '.home',
+			start: '100 0',
+			scrub: true,
+			// markers: true
+		},
+		y: 120,
+		autoAlpha: 0,
+		duration: 1,
+	})
+
+	gsap.from('.wrapper__card-top', {
+		scrollTrigger: {
+			trigger: '.tools',
+			start: '-300 0',
+			scrub: true,
+			// markers: true
+		},
+		scale: 0,
+		duration: 1,
+		stagger: 0.2
+	})
+	gsap.from('.wrapper__card-text', {
+		scrollTrigger: {
+			trigger: '.tools',
+			start: '-300 0',
+			scrub: true,
+			// markers: true
+		},
+		scale: 0,
+		duration: 1,
+		stagger: 0.2
+	})
+	gsap.from('.wrapper__card-bottom', {
+		scrollTrigger: {
+			trigger: '.tools',
+			start: '-300 0',
+			scrub: true,
+			// markers: true
+		},
+		scale: 0,
+		duration: 1,
+		stagger: 0.2
+	})
+	gsap.from('.tools__more', {
+		scrollTrigger: {
+			trigger: '.tools',
+			start: '-300 0',
+			scrub: true,
+			// markers: true
+		},
+		y:120,
+		autoAlpha: 0,
+		duration: 1,
+	})
+	gsap.from('.companions', {
+		scrollTrigger: {
+			trigger: '.tools',
+			start: '100 0',
+			scrub: true,
+			markers: true
+		},
+		y:200,
+		autoAlpha: 0,
+		duration: 1,
 	})
 }
